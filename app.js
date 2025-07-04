3745ccc0456f979a82c0d2ddf42212e452009cde
AliAbdullah1999 <alithedeveloper1999@gmail.com>
2025-07-05 01:28:45 +0500

    Merge remote changes

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine","ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public")); 
+ 
+app.get("/", (req,res)=>{
+    res.render("index",{title: "Home Page"});
+})
+app.get("/about", (req,res)=>{
+    res.render("about",{title: "About Page"});
+})
+app.get("/services", (req,res)=>{
+    res.render("services",{title: "Services Page"});
+})
+app.get("/contact", (req,res)=>{
+    res.render("contact",{title: "Contact Page"});
+})
+app.use((req, res) => {
+    res.status(404).send("404 Not Found - Check your route and file structure.");
+});
+
+app.listen(8000,()=>{
+    console.log("Server is running on port 8000");
+})
